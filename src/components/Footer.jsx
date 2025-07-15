36e3b97906f2e7a7cccd46a63670414a1c175e6e
Nicolas Bomben <nicolasbomben@gmail.com>
2025-07-14 22:30:36 -0300

    [FIX] more responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,34 +5,23 @@ import { FaWhatsapp } from "react-icons/fa";
 import { Container } from "./Container/Container";
 
 export const Footer = () => {
-  //constantes para cuadricula
-  const totalCells = 14 * 7;
-  const cells = Array.from({ length: totalCells });
-
   return (
     <footer className={styles.footerContainer}>
       <Container>
-        {/* div que recrea la celda de fondo */}
-        <div className={styles.gridOverlay}>
-          {cells.map((_, i) => (
-            <div key={i} className={styles.gridCell} />
-          ))}
-        </div>
-
         <h4 className={styles.footerTitle}>CONTACTÁNOS Y SEGUINOS</h4>
 
         <div className={styles.socialWrapper}>
           <div className={styles.socialCard}>
             <FaWhatsapp size={100} className={styles.icon} />
-            <p className={styles.socialText}>+54 9 11 2644-0661</p>
+            <a className={styles.socialText} href="https://wa.me/5491126440661" target="_blank" rel="noopener">+54 9 11 2644-0661</a>
           </div>
           <div className={styles.socialCard}>
             <FaInstagram size={100} className={styles.icon} />
-            <p className={styles.socialText}>fanpro.oficial</p>
+            <a className={styles.socialText} href="https://www.instagram.com/fanpro.oficial/">fanpro.oficial</a>
           </div>
           <div className={styles.socialCard}>
             <FaTiktok size={100} className={styles.icon} />
-            <p className={styles.socialText}>fanpro.oficial</p>
+            <a className={styles.socialText} href="https://www.tiktok.com/@fanprook">fanpro.oficial</a>
           </div>
         </div>
       </Container>
